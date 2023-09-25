f61f0fd142e270432181f1adf0876d1ebaa2b254
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-09-25 19:16:26 +0300

    ed

--- src/components/YourInfo/YourInfo.js
+++ src/components/YourInfo/YourInfo.js
@@ -2,40 +2,66 @@ import { useEffect, useState } from "react";
 import { CurrentNum, Form, Info, Input, Item, ItemBox, Label, LabelText, List, NextBtn, Num, PhoneInput, Step, Text, Title, YourInfoBox} from "./YourInfoStyled"
 
 export const YourInfo = ({onNext}) => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phoneNumber: ''
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: ""
+  });
+  const [validationErrors, setValidationErrors] = useState({
+    name: false,
+    email: false,
+    phoneNumber: false
+  });
+
+  useEffect(() => {
+    const savedFormData = JSON.parse(localStorage.getItem("formData"));
+    if (savedFormData) setFormData(savedFormData);
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+   
+    const isFieldEmpty = value.trim() === '';
+    // setValidationErrors({
+    //   ...validationErrors,
+    //   [name]: value.trim() === ""
+    // });
+
+   
+    if (name === "email") {
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+      setValidationErrors({
+        ...validationErrors,
+        [name]: !emailRegex.test(value)
       });
-    
-      useEffect(() => {
-        const savedFormData = JSON.parse(localStorage.getItem('formData'));
-        if (savedFormData) setFormData(savedFormData);
-      }, []);
-    
-      const handleInputChange = (e) => {
-        const { name, value } = e.target;
+    }
 
-        setFormData({
-          ...formData,
-          [name]: value
-        });
-      };
-    
-      const handleNextClick = () => {
+    if (name === 'phoneNumber') {
+      const phoneRegex = /^\d{10}$/; 
+      setValidationErrors({
+        ...validationErrors,
+        [name]: isFieldEmpty || !phoneRegex.test(value)
+      });
+    }
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
 
-      
-          if (!formData.name.trim() || !formData.email.trim() || !formData.phoneNumber.trim()) {
-            alert('Please fill in all required fields.');
-            return;
-          }
-          if (!formData.email.includes('@')) {
-            alert('Please enter a valid email address.');
-            return;
-          }
-        localStorage.setItem('formData', JSON.stringify(formData));
-        onNext();
-      };
+  const handleNextClick = () => {
+    const hasEmptyFields = Object.values(formData).some((val) => val.trim() === "");
+    const hasValidationErrors = Object.values(validationErrors).some((val) => val);
+
+    if (hasEmptyFields || hasValidationErrors) {
+      alert("Please fill in all required fields with valid data.");
+      return;
+    }
+
+    localStorage.setItem("formData", JSON.stringify(formData));
+    onNext();
+  };
     return(
         <>
         <YourInfoBox>
@@ -74,53 +100,46 @@ export const YourInfo = ({onNext}) => {
             </List>
 
             <div>
-                <Title>Personal Info</Title>
-                <Text>Please provide your name, email address, and phone number.</Text>
-                <Form>
-                    <Label>
-                    <LabelText>
-                        Name    
-                    </LabelText>
-                    <Input 
-                    type="text"
-                    name="name"
-                    placeholder="e.g. Stephen King"
-                    value={formData.name}
-                    onChange={handleInputChange}
-                         
-                    />
-                    
-                    </Label>
-                    <Label>
-                    <LabelText>
-                    Email Address
-                    </LabelText>
-                    <Input 
-                    type="email" 
-                    name="email"
-                    placeholder="e.g. stephenking@lorem.com"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                     
-                    />
-                    </Label>
-                    <Label>
-
-                    <LabelText>Phone Number</LabelText>
-                   
-                    <PhoneInput
-                     name="phoneNumber"
-                     placeholder="e.g. +1 234 567 890" 
-                     type="number"
-                     pattern="[0-9]*" 
-                     value={formData.phoneNumber}  
-                     onChange={handleInputChange} 
-                      
-                     />
-                    </Label>
-                </Form>
-                <NextBtn onClick={handleNextClick}>Next Step</NextBtn>
-            </div>
+          <Title>Personal Info</Title>
+          <Text>Please provide your name, email address, and phone number.</Text>
+          <Form>
+            <Label>
+              <LabelText>Name</LabelText>
+              <Input
+                type="text"
+                name="name"
+                placeholder="e.g. Stephen King"
+                value={formData.name}
+                onChange={handleInputChange}
+                style={{ borderColor: validationErrors.name ? "red" : "" }}
+              />
+            </Label>
+            <Label>
+              <LabelText>Email Address</LabelText>
+              <Input
+                type="email"
+                name="email"
+                placeholder="e.g. stephenking@lorem.com"
+                value={formData.email}
+                onChange={handleInputChange}
+                style={{ borderColor: validationErrors.email ? "red" : "" }}
+              />
+            </Label>
+            <Label>
+              <LabelText>Phone Number</LabelText>
+              <PhoneInput
+                name="phoneNumber"
+                placeholder="e.g. +1 234 567 890"
+                type="number"
+                pattern="[0-9]*"
+                value={formData.phoneNumber}
+                onChange={handleInputChange}
+                style={{ borderColor: validationErrors.phoneNumber ? "red" : "" }}
+              />
+            </Label>
+          </Form>
+          <NextBtn onClick={handleNextClick}>Next Step</NextBtn>
+        </div>
         </YourInfoBox>
         </>
     )
